e782cb4f1e23cdac79e9c7e7775b480a2986d369
Tim <31886035+TimurTimmmy@users.noreply.github.com>
2020-07-14 19:30:17 +0300

    Add files via upload

---
+++ RK/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace RK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.FirstFile = new System.Windows.Forms.TextBox();
+            this.SecondFile = new System.Windows.Forms.TextBox();
+            this.FirstFileButton = new System.Windows.Forms.Button();
+            this.SecondFileButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.GBStoreID = new System.Windows.Forms.GroupBox();
+            this.PsRadio = new System.Windows.Forms.RadioButton();
+            this.RibRadio = new System.Windows.Forms.RadioButton();
+            this.KiwiRadio = new System.Windows.Forms.RadioButton();
+            this.GBStoreID.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(166, 146);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Сформировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FirstFile
+            // 
+            this.FirstFile.Location = new System.Drawing.Point(12, 32);
+            this.FirstFile.Name = "FirstFile";
+            this.FirstFile.Size = new System.Drawing.Size(149, 20);
+            this.FirstFile.TabIndex = 1;
+            // 
+            // SecondFile
+            // 
+            this.SecondFile.Location = new System.Drawing.Point(12, 59);
+            this.SecondFile.Name = "SecondFile";
+            this.SecondFile.Size = new System.Drawing.Size(149, 20);
+            this.SecondFile.TabIndex = 2;
+            // 
+            // FirstFileButton
+            // 
+            this.FirstFileButton.Location = new System.Drawing.Point(181, 32);
+            this.FirstFileButton.Name = "FirstFileButton";
+            this.FirstFileButton.Size = new System.Drawing.Size(75, 23);
+            this.FirstFileButton.TabIndex = 3;
+            this.FirstFileButton.Text = "Исх файл";
+            this.FirstFileButton.UseVisualStyleBackColor = true;
+            this.FirstFileButton.Click += new System.EventHandler(this.FirstFileButton_Click);
+            // 
+            // SecondFileButton
+            // 
+            this.SecondFileButton.Location = new System.Drawing.Point(181, 55);
+            this.SecondFileButton.Name = "SecondFileButton";
+            this.SecondFileButton.Size = new System.Drawing.Size(75, 23);
+            this.SecondFileButton.TabIndex = 4;
+            this.SecondFileButton.Text = "Вх файл";
+            this.SecondFileButton.UseVisualStyleBackColor = true;
+            this.SecondFileButton.Click += new System.EventHandler(this.SecondFileButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // GBStoreID
+            // 
+            this.GBStoreID.Controls.Add(this.PsRadio);
+            this.GBStoreID.Controls.Add(this.RibRadio);
+            this.GBStoreID.Controls.Add(this.KiwiRadio);
+            this.GBStoreID.Location = new System.Drawing.Point(13, 85);
+            this.GBStoreID.Name = "GBStoreID";
+            this.GBStoreID.Size = new System.Drawing.Size(148, 96);
+            this.GBStoreID.TabIndex = 5;
+            this.GBStoreID.TabStop = false;
+            this.GBStoreID.Text = "StoreID";
+            // 
+            // PsRadio
+            // 
+            this.PsRadio.AutoSize = true;
+            this.PsRadio.Location = new System.Drawing.Point(7, 67);
+            this.PsRadio.Name = "PsRadio";
+            this.PsRadio.Size = new System.Drawing.Size(68, 17);
+            this.PsRadio.TabIndex = 2;
+            this.PsRadio.TabStop = true;
+            this.PsRadio.Text = "PaySend";
+            this.PsRadio.UseVisualStyleBackColor = true;
+            this.PsRadio.CheckedChanged += new System.EventHandler(this.PsRadio_CheckedChanged);
+            // 
+            // RibRadio
+            // 
+            this.RibRadio.AutoSize = true;
+            this.RibRadio.Location = new System.Drawing.Point(7, 44);
+            this.RibRadio.Name = "RibRadio";
+            this.RibRadio.Size = new System.Drawing.Size(43, 17);
+            this.RibRadio.TabIndex = 1;
+            this.RibRadio.TabStop = true;
+            this.RibRadio.Text = "RIB";
+            this.RibRadio.UseVisualStyleBackColor = true;
+            this.RibRadio.CheckedChanged += new System.EventHandler(this.RibRadio_CheckedChanged);
+            // 
+            // KiwiRadio
+            // 
+            this.KiwiRadio.AutoSize = true;
+            this.KiwiRadio.Location = new System.Drawing.Point(7, 20);
+            this.KiwiRadio.Name = "KiwiRadio";
+            this.KiwiRadio.Size = new System.Drawing.Size(49, 17);
+            this.KiwiRadio.TabIndex = 0;
+            this.KiwiRadio.TabStop = true;
+            this.KiwiRadio.Text = "KIWI";
+            this.KiwiRadio.UseVisualStyleBackColor = true;
+            this.KiwiRadio.CheckedChanged += new System.EventHandler(this.KiwiRadio_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 192);
+            this.Controls.Add(this.GBStoreID);
+            this.Controls.Add(this.SecondFileButton);
+            this.Controls.Add(this.FirstFileButton);
+            this.Controls.Add(this.SecondFile);
+            this.Controls.Add(this.FirstFile);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "RK";
+            this.GBStoreID.ResumeLayout(false);
+            this.GBStoreID.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox FirstFile;
+        private System.Windows.Forms.TextBox SecondFile;
+        private System.Windows.Forms.Button FirstFileButton;
+        private System.Windows.Forms.Button SecondFileButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.GroupBox GBStoreID;
+        private System.Windows.Forms.RadioButton PsRadio;
+        private System.Windows.Forms.RadioButton RibRadio;
+        private System.Windows.Forms.RadioButton KiwiRadio;
+    }
+}
+
---
+++ RK/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace RK
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            FirstFileButton.Enabled = true;
+            SecondFileButton.Enabled = false;
+            button1.Enabled = false;
+        }
+
+        public string FirstFileString;
+        public string SecondFileString;
+        public string Dir = Environment.CurrentDirectory;
+        public string ShopID;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.Visible = false;
+
+            Excel.Workbook book1 = excelApp.Workbooks.Open(FirstFileString);
+            Excel.Worksheet sheet1 = (Excel.Worksheet)excelApp.Worksheets.get_Item(1);
+            var lastCell1 = sheet1.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);
+            int lastColumn1 = (int)lastCell1.Column;
+            int lastRow1 = (int)lastCell1.Row;
+            string[,] arr1 = new string[lastRow1, lastColumn1];
+
+            Excel.Workbook book2 = excelApp.Workbooks.Open(SecondFileString);
+            Excel.Worksheet sheet2 = (Excel.Worksheet)excelApp.Worksheets.get_Item(1);
+            var lastCell2 = sheet2.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);
+            int lastColumn2 = (int)lastCell2.Column;
+            int lastRow2 = (int)lastCell2.Row;
+            string[,] arr2 = new string[lastRow2, lastColumn2];
+
+            Excel._Workbook NewBook = (Excel._Workbook)(excelApp.Workbooks.Add(Type.Missing));
+            Excel._Worksheet NewSheet = (Excel._Worksheet)NewBook.ActiveSheet;
+            string[,] NewArr = new string[lastRow1, 4];
+
+                        
+            for (int i = 1; i <= lastRow1; i++)
+            {
+                for (int j = 1; j <= lastColumn1; j++)
+                {
+                    arr1[i-1, j-1] = Convert.ToString(sheet1.Cells[i, j].Value);
+                }
+            }
+
+            for (int i = 1; i <= lastRow2; i++)
+            {
+                for (int j = 1; j <= lastColumn2; j++)
+                {
+                    arr2[i - 1, j - 1] = Convert.ToString(sheet2.Cells[i, j].Value);
+                }
+            }
+
+            if (lastRow1 == lastRow2)
+            {
+                NewArr[0, 0] = "Логин";
+                NewArr[0, 1] = "Идентификатор";
+                NewArr[0, 2] = "Ставка";
+                NewArr[0, 3] = "ShopID";
+                for (int i = 1; i < lastRow1; i++)
+                {
+                    if (arr1[i, 1] == arr2[i, 0])
+                    {
+                        NewArr[i, 0] = arr1[i, 0];
+                        NewArr[i, 1] = arr2[i, 22];
+                        NewArr[i, 2] = arr2[i, 7];
+                        NewArr[i, 3] = ShopID;
+                    }
+                }
+            }
+
+            for (int i = 0; i < lastRow1; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    NewSheet.Cells[i+1, j+1] = NewArr[i, j];
+                }
+            }
+
+            //string savepath = string.Format("F:\\RKdocs\\Admin_{0}{1}{2}.xlsx", DateTime.Now.Day, DateTime.Now.Month, DateTime.Now.Year);
+            string savepath = string.Format("{0}\\Admin_{1}{2}{3}.xlsx", Dir, DateTime.Now.Day, DateTime.Now.Month, DateTime.Now.Year);
+            NewBook.SaveAs(savepath); //Сохраняем новую книгу
+            MessageBox.Show("Файл сформирован");
+            book1.Close(false, Type.Missing, Type.Missing); //Закрыть не сохраняя
+            book2.Close(false, Type.Missing, Type.Missing);
+            NewBook.Close(false, Type.Missing, Type.Missing);
+            excelApp.Quit(); //Выйти из экселя
+            GC.Collect(); //Убрать мусор
+            Process.Start(Dir);
+        }
+
+        private void FirstFileButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            FirstFileString = openFileDialog1.FileName;
+            FirstFile.Text = FirstFileString;
+            SecondFileButton.Enabled = true;
+        }
+
+        private void SecondFileButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            SecondFileString = openFileDialog2.FileName;
+            SecondFile.Text = SecondFileString;
+        }
+
+        private void KiwiRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton radioButton = (RadioButton)sender;
+            if (radioButton.Checked)
+            {
+                ShopID = "56893";
+                button1.Enabled = true;
+            }
+        }
+
+        private void RibRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton radioButton = (RadioButton)sender;
+            if (radioButton.Checked)
+            {
+                ShopID = "103370";
+                button1.Enabled = true;
+            }
+        }
+
+        private void PsRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton radioButton = (RadioButton)sender;
+            if (radioButton.Checked)
+            {
+                ShopID = "165290";
+                button1.Enabled = true;
+            }
+        }
+    }
+}
